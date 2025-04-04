0fb5260606d3d8abe77f05c7aa9474c84504d766
jz <391900788@qq.com>
2025-04-05 00:02:34 +0800

    feat: author

--- data/author-info.ts
+++ data/author-info.ts
@@ -1,29 +1,29 @@
 export const AUTHOR_INFO = {
-  name: 'Mengke',
+  name: 'bu44er',
   description:
     'A personal space on the cloud where I document my programming journey, sharing lessons, insights, and resources for fellow developers.',
-  email: 'me@mengke.me',
+  email: '391900788dOtqqDotcom',
   identity: 'Learner | Builder',
   address: {
-    city: 'Peking, China',
+    city: 'Wuhan, China',
     flag: 'flag-china',
     timeZone: 8,
   },
   work: {
     company: '',
     occupation: 'Front-End Engineer',
-    location: 'Peking, China',
+    location: 'Wuhan, China',
     website: '/',
   },
   social: {
-    github: 'https://github.com/mk965',
+    github: 'https://github.com/cynicalight',
     x: '/',
     facebook: '/',
     youtube: '/',
     linkedin: '/',
-    threads: 'https://www.threads.net/@_mk965',
+    threads: '/',
     instagram: '/',
-    juejin: 'https://www.juejin.cn/user/4037062430299912#/Mengke',
+    juejin: '/',
   },
   support: {
     kofi: 'https://ko-fi.com/mengke',
